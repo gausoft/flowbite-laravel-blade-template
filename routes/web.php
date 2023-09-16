b0c8a86cc973bcbe52bf96abadb0cdc59e926765
Gauthier EHOLOUM <thiergau691@gmail.com>
2023-09-16 19:41:07 +0000

    chore: add settings page

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('admin/pages/dashboard');
-})->name('dashboard');
-
-
-Route::get('/users', function () {
-    return view('admin/pages/users');
-})->name('users');
+Route::get('/', fn() => view('admin/pages/dashboard'))->name('dashboard');
+Route::get('/users', fn() => view('admin/pages/users'))->name('users');
+Route::get('/settings', fn() => view('admin/pages/settings'))->name('settings');
